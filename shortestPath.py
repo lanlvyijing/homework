0d1ac39a398b586f4d04891e84eae194a101d2ec
lanlvyijing <lanlvyijing@hotmail.com>
2016-03-04 23:11:48 +0800

    daydayup

--- shortestPath.py
+++ shortestPath.py
@@ -28,42 +28,42 @@ def plotflight(dataset, place):
         height.append(random.randint((i%2)*15, (i%2)*15+5))
         plt.text(height[i]+1, weight[i]+1, '%d'%(i+1), color='blue')   
         plt.plot(height[i], weight[i], 'ro')             
-                    
-    #plt.scatter(height, weight)
-    
+
     for i in range(7):
        
         for j in range(1, i + 1):
             
             if dataset[i-1][j]<100 and dataset[i-1][j]>0:
                 plt.text((height[i-1]+height[j-1])/2, (weight[i-1]+weight[j-1])/2, '%d'%dataset[i-1][j], color='black')
-                plt.plot([height[i-1], height[j-1]], [weight[i-1], weight[j-1]], color='black', linewidth = 1)                
-                               
-        #plt.plot(height[i], weight[i], 'r')
-                                                                       
+                plt.plot([height[i-1], height[j-1]], [weight[i-1], weight[j-1]], color='black', linewidth = 1)                                                                          
     plt.show()
-    #plt.clf()
-   
+
     return weight, height
 
+def fun1(s): return s if s < 100 and s > 0 else None
 
-A = dtype({'names': ['ai', 'tan',  'Ti', 'bn'], 'formats': ['i', 'i' , 'S10', 'int32']}, align = True)
 
-def primgraph(dataset,place):
+A = dtype({'names': ['ai', 'tan', 'bn'], 'formats': ['i', 'i' , 'int32']}, align = True)
+B = dtype({'names': ['bni'], 'formats': ['i']}, align = True)
+
+def primgraph(dataset, place):
     #the adding struct this turn
     dataset, place = file2matrix('flight_price.txt')
-    plotflight(dataset, place)
-    An=[]
-    a = array([(1, 0, "", 0)], dtype = A)
-    print "adding point = a%d ; shortest way to an = %d ; collection of shortest way = %s"%(a[0]['ai'], a[0]['tan'], a[0]['Ti'])
+    #plotflight(dataset, place)
+    An=[];ai=1
+    endpoint = 4 #end point
+    
+    a = array([(1, 0, 0)], dtype = A)
+    print "adding point = a%d ; shortest way to an = %d "%(a[0]['ai'], a[0]['tan'])
     An.append(a[0]['ai'])
+    print An
     print "adding the v%d point to An[],marked that the point has been found\n"%a[0]['ai']
     
     
     print "the distence near this point is:"
     print dataset[a[0]['ai']-1][1:]
     #find the point nearby
-    Nan_value = filter(lambda x: x<100 and x > 0, dataset[a[0]['ai']-1][1:])
+    Nan_value = filter(lambda x: x < 100 and x > 0, dataset[a[0]['ai']-1][1:])
     print "Nan_value:"
     print Nan_value
     
@@ -71,53 +71,81 @@ def primgraph(dataset,place):
     for x in Nan_value:
         if list(dataset[a[0]['ai']-1][1:]).index(x)+1 not in An:
             Nan_index.append(list(dataset[a[0]['ai']-1][1:]).index(x)+1)
-        #Nan_index.append(list(dataset[a[0]['ai']-1][1:]).index(x))
     print "Nan_index:"
     print Nan_index
     
-    #sort the distance to find ai's nearest
-    #b=sort(dataset[a[0]['ai']-1][1:])
+    #print dataset[a[0]['ai']-1][1:]
+    min_bni = min(Nan_value) 
+    vi_index=list(dataset[a[0]['ai']-1][1:]).index(min_bni)+1
+    print 'the nearest point is the %d point,the price is: %d $'%(vi_index, min_bni)
+    a=vstack((a, array([(vi_index, min_bni , 0)], dtype=A)))
     
-    #sort(list(b))
-    print dataset[a[0]['ai']-1][1:]
-    bni = min(Nan_value)
-    
-    print bni
-    vi_index=list(dataset[a[0]['ai']-1][1:]).index(bni)+1
-    print 'the nearest point is the %d point,the price is: %d $'%(vi_index, bni)
-    b = 4
     flag=0
-    while b != ai:
-        flag=flag+1
-        a.append(array([(vi_index, bni, "", 0)]))
-        print "adding point = a%d ; shortest way to an = %d ; collection of shortest way = %s"%(a[flag]['ai'], a[flag]['tan'], a[flag]['Ti'])
-        An.append(a[0]['ai'])
-        print "adding the v%d point to An[],marked that the point has been found\n"%a[0]['ai']
-            
-            
-            print "the distence near this point is:"
-            print dataset[a[0]['ai']-1][1:]
-            #find the point nearby
-            Nan_value = filter(lambda x: x<100 and x > 0, dataset[a[0]['ai']-1][1:])
-            print "Nan_value:"
-            print Nan_value
-            
-            Nan_index=[]
-            for x in Nan_value:
-                if list(dataset[a[0]['ai']-1][1:]).index(x)+1 not in An:
-                    Nan_index.append(list(dataset[a[0]['ai']-1][1:]).index(x)+1)
-                #Nan_index.append(list(dataset[a[0]['ai']-1][1:]).index(x))
-            print "Nan_index:"
-            print Nan_index
-            
-            #sort the distance to find ai's nearest
-            #b=sort(dataset[a[0]['ai']-1][1:])
-            
-            #sort(list(b))
-            print dataset[a[0]['ai']-1][1:]
-            b = min(Nan_value)
-            
-            print b
-            vi_index=list(dataset[a[0]['ai']-1][1:]).index(b)+1        
-
+    ti = {}
+    min_price={}
+       
+    for i in range(2):
+        Nan_value=[] 
+        Nan_index=[]
+        dex = a[i]['ai'][0]-1     
+        for x in dataset[dex][1:]:
+            if x>0 and x< 100 :
+                Nan_value.append(x)
+                Nan_value.sort()
+        for x in Nan_value:
+            if list(dataset[dex][1:]).index(x)+1 not in An:
+                Nan_index.append(list(dataset[dex][1:]).index(x)+1)        
+        
+        print "Nan_value [%d]:"%i 
+        print Nan_value
+        print "Nan_index [%d]:"%i 
+        print Nan_index
+        print "the minest point to a%d is v%d "%(i+1,Nan_index[0])
+                    
+ 
+    '''
+        for x in Nan_value:
+            if list(dataset[a[i]['ai']-1][1:]).index(x)+1 not in An:
+                Nan_index.append(list(dataset[a[0]['ai']-1][1:]).index(x)+1)
+        print "Nan_index [%d]:"%i 
+        print Nan_index   
+       
     
+    bni = array(list(Nan_value),dtype = B)
+    print list(Nan_value)
+    print "bni"
+    print bni[0]['bni']
+    
+    
+    ai_1 = vi_index 
+    while ai_1 != endpoint:
+        flag=flag+1
+        ti["1-%d"%vi_index]=min_bni
+     
+        print ti
+        a=vstack((a, array([(vi_index, min_bni , 0)], dtype=A))) #adding new point's infor        
+        
+        anow=a[flag]['ai'][0]
+        
+        print "adding point = a%d ; shortest way from a1 to a%d = %d "%(anow, anow, a[flag]['tan'])
+        
+        An.append(anow)
+        print "An:"
+        print An
+        print "adding the v%d point to An[],marked that the point has been found\n"%anow
+        
+        print "the point near this point is:"        
+        print dataset[anow-1][1:]
+
+        #find the point nearby
+        Nan_value = filter(lambda x: x<100 and x > 0 and list(dataset[anow-1][1:]).index(x)+1 not in An, dataset[anow-1][1:])
+        print "Nan_value:";print Nan_value                                                  
+        Nan_index=[]
+        for x in Nan_value:
+            Nan_index.append(list(dataset[anow-1][1:]).index(x)+1)        
+        print "Nan_index:";print Nan_index
+        min_bni = min(Nan_value)
+        vi_index=list(dataset[anow-1][1:]).index(min_bni)+1  
+        print 'the nearest point is the %d point,the price is: %d $'%(vi_index, min_bni)      
+        ai_1 = vi_index
+    '''
